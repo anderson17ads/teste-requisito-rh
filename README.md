318abe06008ca8213306e4b16b24f3f254ee24d8
Anderson de Souza <anderson.souza@sennit.com.br>
2019-06-21 11:39:54 -0300

    incluindo o sql e alterando o readme

--- README.md
+++ README.md
@@ -2,16 +2,17 @@
 
 Para que o projeto funcione, siga os passos abaixo:
 
-- Baixar as imagens do Docker 
-	docker-compose up -d
+##Baixar as imagens do Docker 
+###docker-compose up -d
 
-- Baixar as dependências do Composer 
-	a - Primeiro entra na imagem do php-fpm
-			docker exec -it teste-requisito-rh-php-fpm bash
+##Baixar as dependências do Composer 
+###Primeiro entra na imagem do php-fpm
+docker exec -it teste-requisito-rh-php-fpm bash
  	
- 	b - Rode o comando para instalar as dependências do composer
- 			composer install
+###Rode o comando para instalar as dependências do composer
+composer install
 
 --------------------------------
 
+##Banco de dados
 O banco está dentro da pasta src
--- public/teste-requisito-rh.sql
+++ public/teste-requisito-rh.sql
@@ -1,37 +1,112 @@
-CREATE TABLE `usuarios` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `nome` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `senha` varchar(255) NOT NULL
+-- MySQL dump 10.13  Distrib 5.7.26, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: teste-requisito
+-- ------------------------------------------------------
+-- Server version	5.6.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `criticidades`
+--
+
+DROP TABLE IF EXISTS `criticidades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `criticidades` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) NOT NULL,
+  `status` tinyint(1) unsigned NOT NULL,
+  `data_criacao` datetime NOT NULL,
+  `data_alteracao` datetime NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `usuarios`
+-- Dumping data for table `criticidades`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`) VALUES
-(1, 'Anderson de Souza', 'anderson17ads@hotmail.com', '123456'),
-(2, 'Fulano da Silva', 'fulano@hotmail.com', 'a1s2d3f4');
+LOCK TABLES `criticidades` WRITE;
+/*!40000 ALTER TABLE `criticidades` DISABLE KEYS */;
+/*!40000 ALTER TABLE `criticidades` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Indexes for dumped tables
+-- Table structure for table `incidentes`
 --
 
---
--- Indexes for table `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
+DROP TABLE IF EXISTS `incidentes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `incidentes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) NOT NULL,
+  `criticidade_id` int(10) unsigned NOT NULL,
+  `tipo_id` int(10) unsigned NOT NULL,
+  `status` tinyint(1) unsigned NOT NULL,
+  `descricao` text NOT NULL,
+  `data_criacao` datetime NOT NULL,
+  `data_alteracao` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_incidentes_1_idx` (`tipo_id`),
+  KEY `fk_incidentes_1_idx1` (`criticidade_id`),
+  CONSTRAINT `fk_incidentes_1` FOREIGN KEY (`criticidade_id`) REFERENCES `criticidades` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_incidentes_tipos` FOREIGN KEY (`tipo_id`) REFERENCES `tipos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Dumping data for table `incidentes`
 --
 
+LOCK TABLES `incidentes` WRITE;
+/*!40000 ALTER TABLE `incidentes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `incidentes` ENABLE KEYS */;
+UNLOCK TABLES;
+
 --
--- AUTO_INCREMENT for table `usuarios`
+-- Table structure for table `tipos`
 --
-ALTER TABLE `usuarios`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+DROP TABLE IF EXISTS `tipos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tipos` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) NOT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  `data_criacao` datetime NOT NULL,
+  `data_alteracao` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipos`
+--
+
+LOCK TABLES `tipos` WRITE;
+/*!40000 ALTER TABLE `tipos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tipos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-06-21 11:33:17
